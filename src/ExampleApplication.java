43dbd22229f745082ee442730a1cb50ac8fb0b90
Mario Dimitrov <mario.dmtrv@gmail.com>
2014-12-17 16:31:17 +0200

    Initial project setup

---
+++ src/ExampleApplication.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class ExampleApplication extends Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(ProductResource.class);
+		return classes;
+	}
+
+}
