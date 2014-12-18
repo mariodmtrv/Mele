d8d087d3fe1f382bf099f94d9d6f9adb4ac4bba7
Mario Dimitrov <mario.dmtrv@gmail.com>
2014-12-18 13:25:09 +0200

    Added aggregate functions

---
+++ src/org/mele/backend/functions/AggregateFunctions.java
@@ -0,0 +1,29 @@
+package org.mele.backend.functions;
+
+
+import java.text.ParseException;
+
+/**
+ * Created by mariodimitrov on 12/18/14.
+ */
+public class AggregateFunctions<T> {
+    public T min() {
+        return null;
+    }
+
+    public T max() {
+        return null;
+    }
+
+    public T median() {
+        return null;
+    }
+
+    public T mode() {
+        return null;
+    }
+
+    public T sum() {
+        return null;
+    }
+}
---
+++ src/org/mele/tests/functions/AggregateFunctionsTest.java
@@ -0,0 +1,38 @@
+package org.mele.tests.functions;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mariodimitrov on 12/18/14.
+ */
+public class AggregateFunctionsTest {
+    @Test
+    public void testSum() {
+        fail("Not implemented");
+    }
+
+    @Test
+    public void testMin() {
+        fail("Not implemented");
+    }
+
+    @Test
+    public void testMax() {
+        fail("Not implemented");
+    }
+
+    @Test
+    public void testMedian() {
+        fail("Not implemented");
+    }
+
+    @Test
+
+    public void testMode() {
+        fail("Not implemented");
+    }
+}
